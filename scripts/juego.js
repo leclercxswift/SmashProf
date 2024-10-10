dbba0efb4005f79aeccf77019e7e99113f00547f
leclercxswift <fmsmdfr32@gmail.com>
2024-10-09 23:19:28 -0500

    modified:   scripts/juego.js añade ataque básico

--- scripts/juego.js
+++ scripts/juego.js
@@ -5,26 +5,54 @@ const c=canvas.getContext("2d");
 c.fillRect(0,0,canvas.width,canvas.height);
 const gravity=0.7
 class Sprite{
-    constructor({position,velocity}){
+    constructor({position,velocity,color,offset}){
         this.position=position;
         this.velocity=velocity;
+        this.width=50
         this.height=150;
         this.lastKey
+        this.attackBox={
+            position:{
+                x: this.position.x,
+                y: this.position.y
+            },
+            offset,
+            width:100,
+            height:50,
+        }
+        this.color=color
+        this.isAttacking=false
     }
 
     draw(){
-        c.fillStyle="red";
-        c.fillRect(this.position.x,this.position.y,50,this.height);
+        c.fillStyle=this.color;
+        c.fillRect(this.position.x,this.position.y,this.width,this.height);
+
+        //Dibujar el ataque
+        if(this.isAttacking){
+        c.fillStyle="chartreuse"
+        c.fillRect(this.attackBox.position.x,this.attackBox.position.y,this.attackBox.width,this.attackBox.height)
+        }
     }
 
     update(){
         this.draw()
+        this.attackBox.position.x=this.position.x + this.attackBox.offset.x
+        this.attackBox.position.y=this.position.y
+
         this.position.x+=this.velocity.x
         this.position.y+=this.velocity.y
 
         if(this.position.y+this.height+this.velocity.y>=canvas.height){
         this.velocity.y=0
      }else this.velocity.y+=gravity
+    }
+
+    attack(){
+        this.isAttacking=true
+        setTimeout(()=>{
+            this.isAttacking=false
+        },100)
     }
 }
 const player=new Sprite({
@@ -35,6 +63,11 @@ const player=new Sprite({
     velocity:{
         x:0,
         y:10
+    },
+    color:"red",
+    offset:{
+        x:0,
+        y:0
     }
 });
 const enemy=new Sprite({
@@ -44,6 +77,11 @@ const enemy=new Sprite({
     },
     velocity:{
         x:0,
+        y:0
+    },
+    color:"blue",
+    offset:{
+        x:-50,
         y:0
     }
 });
@@ -73,6 +111,11 @@ const keys ={
 
 }
 
+function colisionRectangulo({rectangle1,rectangle2}){
+    return( 
+        rectangle1.attackBox.position.x+rectangle1.attackBox.width>=rectangle2.position.x && rectangle1.attackBox.position.x<=rectangle2.position.x+rectangle2.width && rectangle1.attackBox.position.y+rectangle1.attackBox.height>=rectangle2.position.y && rectangle1.attackBox.position.y<=rectangle2.position.y+rectangle2.height
+    )
+}
 
 function animate(){
     window.requestAnimationFrame(animate)
@@ -96,6 +139,19 @@ function animate(){
     }else if(keys.ArrowRight.pressed && enemy.lastKey==='ArrowRight'){
         enemy.velocity.x=5
     }
+
+    //detecta colisión
+    if( colisionRectangulo({rectangle1:player,rectangle2:enemy})
+        && player.isAttacking){
+        player.isAttacking=false
+        console.log("colision")
+    }
+
+    if( colisionRectangulo({rectangle1:enemy,rectangle2:player})
+        && enemy.isAttacking){
+        enemy.isAttacking=false
+        console.log("t pego el otro")
+    }
 }
 
 
@@ -116,6 +172,10 @@ window.addEventListener("keydown",(event)=>{
         case 'w':
             player.velocity.y=-15
             break
+        case ' ':
+            player.attack()
+            break
+    
         case 'ArrowRight':
             keys.ArrowRight.pressed=true
             enemy.lastKey='ArrowRight'
@@ -126,6 +186,9 @@ window.addEventListener("keydown",(event)=>{
             break
         case 'ArrowUp':
             enemy.velocity.y=-15
+            break
+        case 'Enter':
+            enemy.attack()
             break
     }
 })
