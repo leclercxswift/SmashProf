61f7051cea8b2f236eccc95937e871e488193efb
leclercxswift <fmsmdfr32@gmail.com>
2024-11-18 23:48:43 -0500

    Playable Pablo y Pedro

--- scripts/pj.js
+++ scripts/pj.js
@@ -6,7 +6,7 @@ function createCharacter1(characterData) {
         case "David jajalol":
             return new FighterSprite({
                 position: {
-                    x: 100,
+                    x: 20,
                     y: 620
                 },
                 velocity: {
@@ -51,28 +51,94 @@ function createCharacter1(characterData) {
 
         case "Pablo johelista":
             return new FighterSprite({
-                position: { x: 20, y: 620 },
-                velocity: { x: 0, y: 10 },
-                color: "green",
-                offset: { x: 215, y: 150 },
-                imageSrc: characterData.img,
-                frames: 8,
-                scale: 2.5,
-                sprites: { /* Aquí agregar las imágenes del personaje Pablo */ },
-                attackBox: { offset: { x: 100, y: 50 }, width: 160, height: 50 }
+                position: {
+                    x: 20,
+                    y: 620
+                },
+                velocity: {
+                    x: 0,
+                    y: 10
+                },
+                color: "red",
+                offset: {
+                    x: 50,
+                    y: -15
+                },
+                imageSrc: "../assets/Pablo/Idle.png",
+                frames:1,
+                scale:1.5,
+                sprites: {
+                    idle: {
+                        imageSrc: "../assets/Pablo/Idle.png",
+                        frames: 1
+                    },
+                    run: {
+                        imageSrc: "../assets/Pablo/Run.png",
+                        frames: 9
+                    },
+                    jump: {
+                        imageSrc: "../assets/Pablo/Jump.png",
+                        frames: 3
+                    },
+                    attack1: {
+                        imageSrc: "../assets/Pablo/Attack1.png",
+                        frames: 4
+                    }
+                },
+                attackBox: {
+                    offset: {
+                        x: 40,
+                        y: 50
+                    },
+                    width: 30,
+                    height: 50
+                }
             });
 
         case "Pedro Chau":
             return new FighterSprite({
-                position: { x: 20, y: 620 },
-                velocity: { x: 0, y: 10 },
-                color: "blue",
-                offset: { x: 215, y: 150 },
-                imageSrc: characterData.img,
-                frames: 8,
-                scale: 2.5,
-                sprites: { /* Aquí agregar las imágenes del personaje Pedro */ },
-                attackBox: { offset: { x: 100, y: 50 }, width: 160, height: 50 }
+                position: {
+                    x: 20,
+                    y: 620
+                },
+                velocity: {
+                    x: 0,
+                    y: 10
+                },
+                color: "red",
+                offset: {
+                    x: 50,
+                    y: -15
+                },
+                imageSrc: "../assets/Pedro/Idle.png",
+                frames:1,
+                scale:1.5,
+                sprites: {
+                    idle: {
+                        imageSrc: "../assets/Pedro/Idle.png",
+                        frames: 1
+                    },
+                    run: {
+                        imageSrc: "../assets/Pedro/Run.png",
+                        frames: 9
+                    },
+                    jump: {
+                        imageSrc: "../assets/Pedro/Jump.png",
+                        frames: 3
+                    },
+                    attack1: {
+                        imageSrc: "../assets/Pedro/Attack1.png",
+                        frames: 4
+                    }
+                },
+                attackBox: {
+                    offset: {
+                        x: 40,
+                        y: 50
+                    },
+                    width: 30,
+                    height: 50
+                }
             });
 
         case "olivia rodrigo":
@@ -144,28 +210,94 @@ function createCharacter2(characterData) {
 
         case "Pablo johelista":
             return new FighterSprite({
-                position: { x: 20, y: 620 },
-                velocity: { x: 0, y: 10 },
-                color: "green",
-                offset: { x: 215, y: 150 },
-                imageSrc: characterData.img,
-                frames: 8,
-                scale: 2.5,
-                sprites: { /* Aquí agregar las imágenes del personaje Pablo */ },
-                attackBox: { offset: { x: 100, y: 50 }, width: 160, height: 50 }
+                position: {
+                    x: 1230,
+                    y: 620
+                },
+                velocity: {
+                    x: 0,
+                    y: 10
+                },
+                color: "red",
+                offset: {
+                    x: 50,
+                    y: -15
+                },
+                imageSrc: "../assets/Pablo/IdleLeft.png",
+                frames:1,
+                scale:1.5,
+                sprites: {
+                    idle: {
+                        imageSrc: "../assets/Pablo/IdleLeft.png",
+                        frames: 1
+                    },
+                    run: {
+                        imageSrc: "../assets/Pablo/RunLeft.png",
+                        frames: 9
+                    },
+                    jump: {
+                        imageSrc: "../assets/Pablo/JumpLeft.png",
+                        frames: 3
+                    },
+                    attack1: {
+                        imageSrc: "../assets/Pablo/Attack1Left.png",
+                        frames: 4
+                    }
+                },
+                attackBox: {
+                    offset: {
+                        x: -20,
+                        y: 50
+                    },
+                    width: 30,
+                    height: 50
+                }
             });
 
         case "Pedro Chau":
             return new FighterSprite({
-                position: { x: 20, y: 620 },
-                velocity: { x: 0, y: 10 },
-                color: "blue",
-                offset: { x: 215, y: 150 },
-                imageSrc: characterData.img,
-                frames: 8,
-                scale: 2.5,
-                sprites: { /* Aquí agregar las imágenes del personaje Pedro */ },
-                attackBox: { offset: { x: 100, y: 50 }, width: 160, height: 50 }
+                position: {
+                    x: 1230,
+                    y: 620
+                },
+                velocity: {
+                    x: 0,
+                    y: 10
+                },
+                color: "red",
+                offset: {
+                    x: 50,
+                    y: -15
+                },
+                imageSrc: "../assets/Pedro/IdleLeft.png",
+                frames:1,
+                scale:1.5,
+                sprites: {
+                    idle: {
+                        imageSrc: "../assets/Pedro/IdleLeft.png",
+                        frames: 1
+                    },
+                    run: {
+                        imageSrc: "../assets/Pedro/RunLeft.png",
+                        frames: 9
+                    },
+                    jump: {
+                        imageSrc: "../assets/Pedro/JumpLeft.png",
+                        frames: 3
+                    },
+                    attack1: {
+                        imageSrc: "../assets/Pedro/Attack1Left.png",
+                        frames: 4
+                    }
+                },
+                attackBox: {
+                    offset: {
+                        x: -20,
+                        y: 50
+                    },
+                    width: 30,
+                    height: 50
+                }
             });
 
         case "olivia rodrigo":
